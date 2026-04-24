88239f26973043c17a5ec20b1b620a4f3b04492e
Jean-Francois Dockes <jf@dockes.org>
2026-04-24 09:41:18 +0200

    appimage: fixed script to run on VM.

--- packaging/appimage/makeimage.sh
+++ packaging/appimage/makeimage.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# MUST run from the directory where the build happens (distrohome/deb11 or ~/appimage-build on the VM
+
 # Note: this supposes that all used python version have been built
 # from source and installed with make altinstall, and that a
 # py3versions program is found in the PATH before the system one, and
@@ -13,27 +15,29 @@ fatal()
     exit 1
 }
 
-DOCKHOME=/home/dockes/tmp/distrohomes/deb11
+BUILDTOP=`pwd`
 RCLDISTS=/home/dockes/projets/fulltext/web-recoll
 RCLSRC=/home/dockes/projets/fulltext/recoll
 
 FROMGIT=0
 RECOLL_VERSION=1.43.14
 
-UNRTF=$DOCKHOME/unrtf-0.21.11
-ANTIWORD=$DOCKHOME/antiword
+UNRTF=unrtf-0.21.11
+ANTIWORD=antiword
 
-APPDIR=$DOCKHOME/AppDir/
-DEPLOYBINDIR=$DOCKHOME/.local/bin
-BUILDDIR=$DOCKHOME/build
+DEPLOYBINDIR=~/.local/bin
+
+APPDIR=$BUILDTOP/AppDir/
+BUILDDIR=$BUILDTOP/recoll-build
 # NOTE: delete the builddir when changing versions. Not done automatically because complicated
 #rm -rf $BUILDDIR
 
 if test $FROMGIT -ne 0; then
     RECOLL=/home/dockes/projets/fulltext/recoll/src
 else
-    RECOLL=$DOCKHOME/recoll-${RECOLL_VERSION}
-    test -d $RECOLL || (cd $DOCKHOME && tar xf $RCLDISTS/recoll-${RECOLL_VERSION}.tar.gz) || \
+    # TBD: Replace this with curl or wget from the web site so we don't need a local copy on the VM ?
+    RECOLL=$BUILDTOP/recoll-${RECOLL_VERSION}
+    test -d $RECOLL || (cd $BUILDTOP && tar xf $RCLDISTS/recoll-${RECOLL_VERSION}.tar.gz) || \
         fatal source extraction
 fi
 
@@ -42,15 +46,16 @@ auxprogs()
     # This works because:
     # - We change the apps to find their resources relative to the exec. Ok for antiword and unrtf
     # - We make sure that the PATH is ok for the handlers to find them in the mounted bin
-
+    cd  || exit 1
+    
     echo "Building UNRTF"
-    cd $UNRTF
+    cd $BUILDTOP/$UNRTF || exit 1
     ./configure --prefix=/usr
     make -j 4 || exit 1
     make install DESTDIR=$APPDIR || exit 1
 
     echo "Building ANTIWORD"
-    cd $ANTIWORD
+    cd $BUILDTOP/$ANTIWORD
     make -f Makefile.Linux
     cp antiword $APPDIR/usr/bin/ || exit 1
     mkdir -p $APPDIR/usr/share/antiword || exit 1
@@ -76,14 +81,14 @@ DESTDIR=$APPDIR ninja -C ${BUILDDIR} install || exit 1
 echo;echo ALSO INSTALLING TO /usr FOR PYTHON BUILDS
 sudo ninja -C ${BUILDDIR} install || exit 1
 
-echo;Copying Python binary extension shared objects
+echo Copying Python binary extension shared objects
 cp ${BUILDDIR}/python/recoll/*.so $APPDIR/usr/lib/python3/dist-packages/recoll || exit 1
 cp ${BUILDDIR}/python/pychm/*.so $APPDIR/usr/lib/python3/dist-packages/recollchm || exit 1
 cp ${BUILDDIR}/python/pyaspell/*.so $APPDIR/usr/lib/python3/dist-packages/ || exit 1
 
 auxprogs
 
-cd
+cd $BUILDTOP
 $DEPLOYBINDIR/linuxdeploy-x86_64.AppImage \
     --appdir $APPDIR \
     --custom-apprun=$RCLSRC/packaging/appimage/AppRun \
