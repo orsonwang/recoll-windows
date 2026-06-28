52806313fff9dcde112bf545315597414cfb68de
Jean-Francois Dockes <jf@dockes.org>
2026-06-28 10:58:47 +0200

    pdf-annot test: work around issue with ubuntu resolute base64 command

--- tests/pdf-annots/pdf-annots.sh
+++ tests/pdf-annots/pdf-annots.sh
@@ -12,7 +12,11 @@ initvariables $0
     # This supposes that the fields file is customized, which is not the case by default
     echo 
     echo "Extracting the value for an annotation field:"
-    recollq -F annotation pdfannot:'"DAVID: Test of a highlight"'  |  tail -1 | base64 -d
+    # For some reason as of ubuntu resolute, the base64 -d command
+    # which used to work identifies the input as wrong. Replaced by
+    # python script.
+    recollq -F annotation pdfannot:'"DAVID: Test of a highlight"'  |  tail -1 |  \
+      python3 -c "import base64;import sys;print(base64.b64decode(sys.stdin.read()).decode('utf-8'))"
 
 )  2> $mystderr | grep -E -a -v '^Recoll query: ' > $mystdout
 
